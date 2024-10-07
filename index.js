b9fb531af92ce1ca16404f172e22e7c90f548612
MATIN SANGABI <sangabiwork@gmail.com>
2024-10-07 21:50:13 +0330

    feat : add vercel and update port and add mongo atlas

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const Application = require('./app/server')
 
-const Port = process.env.PORT
+const Port = process.env.NODE_ENV === "production" ? 3000 :  process.env.PORT
 const DB_URL = process.env.DB_URL
 const Cookie_parser = process.env.COOKIE_PARSER_SECRET_KEY
 
